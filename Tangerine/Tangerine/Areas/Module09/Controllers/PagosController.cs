e08cb8b1eb02b42cc458b3f09ee3a9640c24e196
Roysbert Salinas <rasc.19@gmail.com>
2016-04-10 19:22:25 -0430

    Ejemplo de uso del template.

---
+++ Tangerine/Tangerine/Areas/Module09/Controllers/PagosController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tangerine.Areas.Module09.Controllers
+{
+    public class PagosController : Controller
+    {
+        //
+        // GET: /Module09/Pagos/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
